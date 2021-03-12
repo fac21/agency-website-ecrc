51929d5f796dac86ced9e4173bafe0e1730941a5
chunzg <chunzhang53@gmail.com>
2021-03-12 11:50:09 +0000

    Move toggle to navbar

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const toggleSwitch = document.querySelector(
     if (e.target.checked) {
       document.documentElement.classList.add("dark");
       document.getElementById('title').style.color = "#d3d3d3";
+      document.querySelector('.nav li a').style.color = 'white';
+      document.querySelector('button').style.background = 'white';
     } else {
       document.documentElement.classList.remove("dark");
     }
