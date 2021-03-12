20a1992dbfeace31f806a1c031d3661887ae36d8
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-03-12 11:49:53 +0000

    definiting title variable outside of function
    relates #22

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const toggleSwitch = document.querySelector(
     '.theme-switch input[type="checkbox"]'
   );
 
+let title = document.getElementById('title');
+
   function switchTheme(e) {
     if (e.target.checked) {
       document.documentElement.classList.add("dark");
-      document.getElementById('title').style.color = "#d3d3d3";
+      title.style.color = "#d3d3d3";
     } else {
       document.documentElement.classList.remove("dark");
     }
